cdbd96deb807aa0f9c0d68a3f24241b202b90bc5
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-12-31 15:18:53 +0800

    bot->client refactor

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='.', intents=intents)
+client = commands.Bot(command_prefix='.', intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'Bot is now online. Ping is {round(bot.latency * 1000)}ms.')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("D&D 5e"))
+    print(f'Bot is now online. Ping is {round(client.latency * 1000)}ms.')
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     """Gets the latency of the bot."""
-    await ctx.send(f'pong. {round(bot.latency * 1000)}ms.')
+    await ctx.send(f'pong. {round(client.latency * 1000)}ms.')
 
-@bot.command()
+@client.command()
 async def roll(ctx, *args):
     try:
         return await ctx.reply(str(d20.roll(''.join(args))))
@@ -33,4 +34,4 @@ async def roll(ctx, *args):
     except d20.TooManyRolls:
         return await ctx.reply("you roll the dice and it spills all over the floor, you rolled too much dice.")
 
-bot.run(os.getenv('TOKEN'))
+client.run(os.getenv('TOKEN'))
