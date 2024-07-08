cead7d26dd20934c1ec963cc481465d2b90a701e
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-07-08 18:08:26 +0800

    add more tests for macro

--- test.py
+++ test.py
@@ -7,6 +7,20 @@ class MacroTests(unittest.TestCase):
         result = do_macro("$0+3", ["1"])
         self.assertEqual(result.total, 4)
 
+    def test_macro_rng(self):
+        result = do_macro("$0+1", ["1d6"])
+        self.assertGreaterEqual(result.total, 2)
+
+    def test_macro_multiarg(self):
+        result = do_macro("$0+$1+$2+$3+$4", ['1', '2', '3', '4', '5'])
+        self.assertEqual(result.total, 15)
+        result = do_macro("$0-$1+$2*$3/$4", ['1', '2', '3', '4', '5'])
+        self.assertEqual(result.total, 1)
+
+    def test_macro_double_rng(self):
+        result = do_macro("$0+$1", ["1d6", "1d4"])
+        self.assertGreaterEqual(result.total, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
