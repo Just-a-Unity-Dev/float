b49e7a66a2b7ef670befe8350f885ff582466d25
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-07-08 18:00:59 +0800

    macro implementation

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import unittest
+from classes.macro import do_macro
 
 
-class ExampleTests(unittest.TestCase):
-    def test_example(self):
-        x = 0
-        self.assertEqual(x, 0)
+class MacroTests(unittest.TestCase):
+    def test_macro_basic(self):
+        result = do_macro("$0+3", ["1"])
+        self.assertEqual(result.total, 4)
 
 
 if __name__ == '__main__':
